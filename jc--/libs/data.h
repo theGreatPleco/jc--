92ef825f60688703e21dc90ceff526f24f42e3d9
theGreatPleco <reavon010@outlook.com>
2024-04-27 01:39:22 -0700

    added varibles neccesary for compiling

--- jc--/libs/data.h
+++ jc--/libs/data.h
@@ -1,13 +1,15 @@
 #include "../decl.h"
+#include "stdio.h"
+#ifndef DATA_H
+#define DATA_H
 
-#ifndef DATA_H 
-  #define DATA_H 
-
-
-#ifndef extern_ 
-  #define extern_ extern
-#endif 
+#ifndef extern_
+#define extern_ extern
+#endif
 
 extern_ token *toks;
+extern_ byte putback;
+extern_ u32 line;
+extern_ FILE *STREAM;
 
 #endif
